cf9f1b435de81b669f11418667084d93158ee763
waltaskew <waltaskew@gmail.com>
2012-10-20 16:30:11 -0500

    add parser for the prophecy html

---
+++ prophetic_parser.py
@@ -0,0 +1,41 @@
+"""Get a list of prophecies from the html in a directory."""
+
+
+import lxml.html
+import os
+import re
+
+
+PROPHECY_DIR = 'prophecy_html'
+PROPHECY_SPLIT_RE = re.compile('[\n]*^[\d]+\n', flags=re.MULTILINE)
+
+def iter_prophecy_files(prophecy_dir):
+    """Return a generator yielding the text of all the files
+    in @prophecy_dir.
+    """
+    for file_name in os.listdir(prophecy_dir):
+        yield os.path.join(prophecy_dir, file_name)
+
+def get_text(file_name):
+    """Seperate the prophecy text from html noise and return the text."""
+    with open(file_name, 'r') as prophetic_html:
+        tree = lxml.html.parse(prophetic_html)
+        root = tree.getroot()
+        text = root.xpath('body/pre')[0].text_content()
+        return text.encode('ascii', 'ignore')
+
+def parse_prophecies(text):
+    """Return a list of prophecies from a big text blob containing
+    a series of them.
+    """
+    prophecies = PROPHECY_SPLIT_RE.split(text)
+    prophecies = [prophecy.strip() for prophecy in prophecies]
+    return [prophecy for prophecy in prophecies if prophecy]
+
+def get_prophecies(prophecy_dir):
+    """Get a list of prophecies from the html in @prophecy_dir."""
+    all_prophecies = []
+    for prophecy_file in iter_prophecy_files(prophecy_dir):
+        text = get_text(prophecy_file)
+        all_prophecies.extend(parse_prophecies(text))
+    return all_prophecies
